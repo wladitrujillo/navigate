cd4e08ac1947bbc882920a160b496a856670c67f
Nelson Trujillo <nelson@localhost.localdomain>
2020-04-20 19:33:42 -0500

    NTR-feat:init commit

---
+++ screens/DetalleCompra.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+export class DetalleCompra extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>DETALLE DE COMPRA</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

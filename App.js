29cee10d180806a72e2e03d1898ca0991775f00e
Nelson Trujillo <nelson@localhost.localdomain>
2020-04-20 21:05:55 -0500

    add tab compra

--- App.js
+++ App.js
@@ -2,13 +2,29 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Icon } from 'react-native-elements';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { DetalleCompra } from "./screens/DetalleCompra";
 import { ListaCompras } from "./screens/ListaCompras";
 import { ListaProductos } from "./screens/ListaProductos";
 import { FormularioProducto } from "./screens/FormularioProducto";
 
 let NavStack = createStackNavigator();
 let NavTab = createBottomTabNavigator();
+
+
+let TabCompra = () => {
+  return <NavStack.Navigator initialRouteName="FormularioScreen">
+    <NavStack.Screen
+      name="DetalleCompra"
+      component={DetalleCompra} options={{ title: "Detalle Compra" }}
+    />
+    <NavStack.Screen
+      name="FormularioScreen"
+      component={FormularioProducto} options={{ title: "Formulario de Productos" }}
+    />
+  </NavStack.Navigator>
+}
 
 export default function App() {
   return (
@@ -20,33 +36,35 @@ export default function App() {
             tabBarLabel: "Compras",
             tabBarIcon: () => {
               return <Icon
-                name='sc-telegram'
+                name='cart-arrow-down'
                 type='evilicon'
                 color='#517fa4'
               />
             }
           }}></NavTab.Screen>
         <NavTab.Screen name="ListaProductosScreen" component={ListaProductos}
-          options={{ tabBarLabel: "Productos" }}></NavTab.Screen>
+          options={{
+            tabBarLabel: "Productos",
+            tabBarIcon: () => {
+              return <Icon
+                name='bitbucket'
+                type='evilicon'
+                color='#517fa4'
+              />
+            }
+          }}></NavTab.Screen>
+        <NavTab.Screen name="StackScreen" component={TabCompra}
+          options={{
+            tabBarLabel: "TabCompra",
+            tabBarIcon: () => {
+              return <Icon
+                name='bitbucket'
+                type='evilicon'
+                color='#517fa4'
+              />
+            }
+          }}></NavTab.Screen>
       </NavTab.Navigator>
-
-
-      {
-        /*<NavStack.Navigator initialRouteName="ListaComprasScreen">
-          <NavStack.Screen
-            name="ListaComprasScreen"
-            component={ListaCompras} options={{ title: "Lista de Compras" }}
-          />
-          <NavStack.Screen
-            name="ListaProductosScreen"
-            component={ListaProductos} options={{ title: "Lista de Productos" }}
-          />
-          <NavStack.Screen
-            name="FormularioProductosScreen"
-            component={FormularioProducto} options={{ title: "Formulario de Productos" }}
-          />
-        </NavStack.Navigator>*/
-      }
     </NavigationContainer>
   );
 }
--- screens/FormularioProducto.js
+++ screens/FormularioProducto.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, View, Button } from "react-native";
 export class FormularioProducto extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>FORMULARIO DE PROUDCTO</Text>
+        <Button
+          title="Detalle Compra"
+          onPress={() => { this.props.navigation.navigate("DetalleCompra") }} />
       </View>
     );
   }

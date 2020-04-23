e4f127368cce1c5520c494e01316051c7ebd49e7
Nelson Trujillo <nelson@localhost.localdomain>
2020-04-22 22:27:37 -0500

    add styles

--- screens/FormularioProducto.js
+++ screens/FormularioProducto.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 export class FormularioProducto extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      name: "",
+      price: ""
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>FORMULARIO DE PROUDCTO</Text>
-       
+        <Input
+          placeholder="Id"
+          value={this.state.id}
+          onChangeText={(id) => { this.setState({ id: id }) }} />
+        <Input
+          placeholder="Nombre"
+          value={this.state.name}
+          onChangeText={(name) => { this.setState({ name: name }) }}
+          secureTextEntry={true}
+        />
+        <Input
+          placeholder="Precio"
+          value={this.state.price}
+          onChangeText={(price) => { this.setState({ price: price }) }}
+          secureTextEntry={true}
+        />
+        <Button
+          title="Registrar"
+          onPress={() => {
+            Alert.alert("Click");
+          }}
+        />
       </View>
     );
   }
--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { NavigationContext } from '@react-navigation/native';
-import { Input } from "react-native-elements"
-import { validarIngreso } from "../services/servicios.login"
+import { Input } from "react-native-elements";
+import { validarIngreso } from "../services/servicios.login";
+import { Button } from "react-native-elements";
 export class Login extends Component {
     static contextType = NavigationContext;
 
@@ -19,57 +20,72 @@ export class Login extends Component {
         navigation.navigate("RecuperarClave")
     }
 
-    
+
 
     render() {
         const navigation = this.context;
         return (
             <View style={styles.container}>
-                
-                <Input
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChangeText={(email) => { this.setState({ email: email }) }} />
-                <Input
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChangeText={(password) => { this.setState({ password: password }) }}
-                    secureTextEntry={true}
-                />
-                <Button
-                    title="Ingresar"
-                    onPress={() => { 
-                        validarIngreso(this.state.email, this.state.password, this.toHome);
-                     }}
-                />
-                <Button
-                    title="Registrar"
-                    onPress={() => { navigation.navigate("Registrarse") }}
-                />
-                <Button
-                    title="Recuperar Clave"
-                    onPress={() => { navigation.navigate("RecuperarClave") }}
-                />
+
+                <View style={styles.logo}>
+
+                </View>
+
+                <View style={styles.form}>
+                    <Input
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChangeText={(email) => { this.setState({ email: email }) }} />
+                    <Input
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChangeText={(password) => { this.setState({ password: password }) }}
+                        secureTextEntry={true}
+                    />
+                </View>
+                <View style={styles.botones}>
+
+
+                    <Button               
+                        title="Ingresar"
+                        onPress={() => {
+                            validarIngreso(this.state.email, this.state.password, this.toHome);
+                        }}
+                    />
+                    <Button                       
+                        title="Registrar"
+                        onPress={() => { navigation.navigate("Registrarse") }}
+                    />
+                    <Button                        
+                        title="Recuperar Clave"
+                        onPress={() => { navigation.navigate("RecuperarClave") }}
+                    />
+                </View>
+
+
+
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {      
+    container: {
         backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+        flex: 1,
+        flexDirection: "column"
     },
     logo: {
         flex: 3
     },
     form: {
-        flex: 4,
-        //flexDirection: 'column'
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 2
     },
     botones: {
-        flex: 2,
-        justifyContent: 'center',
+        flex: 3,        
+        flexDirection: "column"
+
     }
 });

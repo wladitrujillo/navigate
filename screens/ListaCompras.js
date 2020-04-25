3d81b4fd972ea98db0982d7bd80c6e63046ab3a9
Nelson Trujillo <nelson@localhost.localdomain>
2020-04-24 21:26:20 -0500

    add lista products

--- screens/ListaCompras.js
+++ screens/ListaCompras.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { registrarListener } from "../services/servicios.product";
 
 export class ListaCompras extends Component {
+
+  constructor() {
+    super();
+
+
+    this.state = {
+      products: []
+    }
+
+
+  }
+
+  componentDidMount() {
+    registrarListener(this.pintarLista);
+  }
+
+  pintarLista = (arregloProducts) => {
+    this.setState({
+      products: arregloProducts
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>LISTA DE COMPRAS</Text>
+        <FlatList
+          data={this.state.products}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+          keyExtractor={product => product.id + ""} >
+        </FlatList>
         <Button
           title="Detalle Compra"
-          onPress={() => { this.props.navigation.navigate("DetalleCompraScreen") }} />      
+          onPress={() => { this.props.navigation.navigate("DetalleCompraScreen") }} />
       </View>
     );
   }
